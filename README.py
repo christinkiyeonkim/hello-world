ff312629588ff05b628dfca6ad26946ccc533d22
christinkiyeonkim <christinkiyeonkim@gmail.com>
2025-03-27 12:41:42 -0500

    first python script

---
+++ README.py
@@ -0,0 +1,20 @@
+print("Hello, World!")
+print("This is a script practice for python while taking Coursera: Google IT Automation with Python.")
+
+
+
+
+name="Christin"
+print("Hello" + name)
+
+print(5+5)
+print(2*5)
+print(4/2)
+print(1/2)
+print(2**10)
+print(((2050/5)-32)/9)
+
+
+print("I hope this works well")
+
+
