5c60cf224ef1d5acdd1dc97fa74dd19030c641ab
christinkiyeonkim <christinkiyeonkim@gmail.com>
2025-03-29 00:14:53 -0500

    This is my second python practice script about if else and elif.

---
+++ ifelseelif.py
@@ -0,0 +1,45 @@
+print("Hello, World!")
+
+
+print("This is Christin Kiyeon Kim")
+
+print("I'm studying python via coursera and this is my practice log that I learned")
+
+print("Today, I learned if, else, and elif.")
+
+print("Following is the scenario I find it useful from the coursera.")
+
+
+
+
+def hint_username(username):
+    if len(username)<3:
+        print("Invalid username. Must be at least 3 characters long.")
+
+
+def hint_username(username):
+    if len(username)<3:
+        print("Invalid username. Must be at least 3 characters long.")
+    else:
+        print("Valid username")
+
+
+
+#I also learned to use hashtag to add comments.
+
+
+def hint_username(username):
+    if len(username)<3:
+        print("Invalid username. Must be at least 3 characters long.")
+    elif len(username)>15:
+        print("Invalid username. Must be at most 15 characters long.")
+    else:
+        print("Valid username")
+
+
+# on if statements, computer checks line to line and if it applies, then computer will disregard the rest. 
+
+
+print("That is all the note for if, else, and elif.")
+
+
