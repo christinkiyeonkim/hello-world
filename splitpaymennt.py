deb10762140f1ee214148752431c6254934ad7c8
christinkiyeonkim <christinkiyeonkim@gmail.com>
2025-03-29 21:42:05 -0500

    This is my python practice log2 from coursera. I did it from my top of my head without using the note :D

---
+++ splitpaymennt.py
@@ -0,0 +1,19 @@
+print("Welcome to my python practice log 2.")
+print("In this log, I'm going to practice spliting the bill scenario from the coursera.")
+print("I'm going to try from top of my head without using the notes")
+
+print("scenario: There are two friends A and B the total bill is $60 and the tip is 18%.")
+
+bill = 60
+percent = 0.18
+tip = (bill * percent) 
+ 
+total = (bill + tip)
+
+print("each person will pay " + str(total/2))
+
+
+
+
+
+
